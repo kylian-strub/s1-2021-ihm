16a5663f047bd6f641662078f118d8ec27b6d6bf
timcarrara <145650887+timcarrara@users.noreply.github.com>
2023-10-03 11:17:20 +0200

    Add files via upload

---
+++ interaction_fleches.js
@@ -0,0 +1,20 @@
+const slide = ["IMAGE/Image actu 1.jpg", "IMAGE/image_actu_2.jpg", "IMAGE/Image actu 3.jpg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+
+document.getElementById("slide").src = slide[numero];
+
+const legendes = document.querySelectorAll('.legende');
+legendes.forEach(legende => {
+    legende.classList.remove('active');
+});
+
+legendes[numero].classList.add('active');
+}
